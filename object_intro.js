02b3d958e3a21deb8d2818b89abf2c663f99a8fc
adityasunny1189 <adityapathak1189@gmail.com>
2019-09-23 06:45:49 +0530

    basics of object

---
+++ object_intro.js
@@ -0,0 +1,11 @@
+//Declaring object
+var object = {
+    propertyOne : "this is property one",
+    propertyTwo : "New property",
+    propertyThree : 3,
+    propFour : true
+};
+
+console.log(object.propertyOne);
+console.log(object.propertyTwo);
+console.log(object.propFour);
